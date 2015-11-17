7a8d64cde4caa017cf11bf941b32738a2ae2494a
stephaneleprevost <stephane.leprevost@devinci.fr>
2015-11-17 15:15:20 +0100

    step 5

--- lib/app.jsx
+++ lib/app.jsx
@@ -13,7 +13,23 @@ var Box = React.createClass({
    */
    
    getInitialState: function() {
-    return {value: this.props.initialvalue};
+    return {value: this.props.initialValue};
+  },
+  
+  componentWillMount: function ()
+   {
+   setInterval(this.updatetime,300);
+   },
+   updatetime: function()
+  {
+   if(this.state.value == 'X')
+   {
+	return this.setState({value:'O'});
+   }
+   else
+   {
+   return this.setState({value:'X'});
+   }
   },
   
   'render': function onRender () {
@@ -23,4 +39,4 @@ var Box = React.createClass({
   }
 });
 
-React.render(<Box initialvalue='X'/>, document.body);
+React.render(<Box initialValue='X'/>, document.body);
